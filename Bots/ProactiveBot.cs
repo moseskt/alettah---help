5503f9d29076afa2de23f5678d123d849335d062
Moses <moses.tapfuma@omtestwintech.onmicrosoft.com>
2020-10-27 15:22:28 +0200

    Initial Commit

---
+++ Bots/ProactiveBot.cs
@@ -0,0 +1,336 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.BotBuilderSamples
+{
+    //public class ProactiveBot : ActivityHandler
+    public class ProactiveBot : TeamsActivityHandler
+    {
+        private string _appId;
+        private string _appPassword;
+
+        // Message to send to users when the bot receives a Conversation Update event
+        private const string WelcomeMessage = "Welcome to the Proactive Bot sample.  Navigate to /api/notify to proactively message everyone who has previously interacted with this bot.";
+
+        // Dependency injected dictionary for storing ConversationReference objects used in NotifyController to proactively message users
+        private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferences;
+
+        public ProactiveBot(IConfiguration configuration, ConcurrentDictionary<string, ConversationReference> conversationReferences)
+        {
+            _conversationReferences = conversationReferences;
+            _appId = configuration["MicrosoftAppId"];
+            _appPassword = configuration["MicrosoftAppPassword"];
+        }
+
+        private void AddConversationReference(Activity activity)
+        {
+            var conversationReference = activity.GetConversationReference();
+            _conversationReferences.AddOrUpdate(conversationReference.User.Id, conversationReference, (key, newValue) => conversationReference);
+        }
+
+        protected override Task OnConversationUpdateActivityAsync(ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            AddConversationReference(turnContext.Activity as Activity);
+
+            return base.OnConversationUpdateActivityAsync(turnContext, cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            AddConversationReference(turnContext.Activity as Activity);
+
+            foreach (var member in membersAdded)
+            {
+                // Greet anyone that was not the target (recipient) of this message.
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(WelcomeMessage), cancellationToken);
+                }
+            }
+        }
+
+        //protected override async Task OnTeamsMembersAddedAsync(IList<TeamsChannelAccount> membersAdded, TeamInfo teamInfo, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        //{
+        //    foreach (var teamMember in membersAdded)
+        //    {
+        //        await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome to the team {teamMember.GivenName} {teamMember.Surname}."), cancellationToken);
+        //    }
+        //}
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            AddConversationReference(turnContext.Activity as Activity);
+            turnContext.Activity.RemoveRecipientMention();
+            var text = turnContext.Activity.Text.Trim().ToLower();
+
+            if (text.Contains("mention"))
+                await MentionActivityAsync(turnContext, cancellationToken);
+
+            else if (text.Contains("who"))
+                await GetSingleMemberAsync(turnContext, cancellationToken);
+
+            else if (text.Contains("message"))
+                await MessageAllMembersAsync(turnContext, cancellationToken);
+
+            else if (text.Contains("respond"))
+                await RespondToThreadAsync(turnContext, cancellationToken);
+
+            else if (text.Contains("delete"))
+                await DeleteCardActivityAsync(turnContext, cancellationToken);
+
+            else if (text.Contains("update"))
+                await CardActivityAsync(turnContext, true, cancellationToken);
+
+            else
+                await CardActivityAsync(turnContext, false, cancellationToken);
+
+            //AddConversationReference(turnContext.Activity as Activity);
+
+            // Echo back what the user said
+            //await turnContext.SendActivityAsync(MessageFactory.Text($"You sent '{turnContext.Activity.Text}'"), cancellationToken);
+        }
+
+        private async Task CardActivityAsync(ITurnContext<IMessageActivity> turnContext, bool update, CancellationToken cancellationToken)
+        {
+
+            var card = new HeroCard
+            {
+                Buttons = new List<CardAction>
+                        {
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Message all members",
+                                Text = "MessageAllMembers"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Who am I?",
+                                Text = "whoami"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Delete card",
+                                Text = "Delete"
+                            }
+                        }
+            };
+
+
+            if (update)
+            {
+                await SendUpdatedCard(turnContext, card, cancellationToken);
+            }
+            else
+            {
+                await SendWelcomeCard(turnContext, card, cancellationToken);
+            }
+
+        }
+
+        private async Task GetSingleMemberAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var member = new TeamsChannelAccount();
+
+            try
+            {
+                member = await TeamsInfo.GetMemberAsync(turnContext, turnContext.Activity.From.Id, cancellationToken);
+            }
+            catch (ErrorResponseException e)
+            {
+                if (e.Body.Error.Code.Equals("MemberNotFoundInConversation"))
+                {
+                    await turnContext.SendActivityAsync("Member not found.");
+                    return;
+                }
+                else
+                {
+                    throw e;
+                }
+            }
+
+            var message = MessageFactory.Text($"You are: {member.Name}.");
+            var res = await turnContext.SendActivityAsync(message);
+
+        }
+
+        private async Task DeleteCardActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.DeleteActivityAsync(turnContext.Activity.ReplyToId, cancellationToken);
+        }
+
+        // If you encounter permission-related errors when sending this message, see
+        // https://aka.ms/BotTrustServiceUrl
+        private async Task MessageAllMembersAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var teamsChannelId = turnContext.Activity.TeamsGetChannelId();
+            var serviceUrl = turnContext.Activity.ServiceUrl;
+            var credentials = new MicrosoftAppCredentials(_appId, _appPassword);
+            ConversationReference conversationReference = null;
+
+            var members = await GetPagedMembers(turnContext, cancellationToken);
+
+            foreach (var teamMember in members)
+            {
+                var proactiveMessage = MessageFactory.Text($"Hello {teamMember.GivenName} {teamMember.Surname}. I'm a Teams conversation bot.");
+
+                var conversationParameters = new ConversationParameters
+                {
+                    IsGroup = false,
+                    Bot = turnContext.Activity.Recipient,
+                    Members = new ChannelAccount[] { teamMember },
+                    TenantId = turnContext.Activity.Conversation.TenantId,
+                };
+
+                await ((BotFrameworkAdapter)turnContext.Adapter).CreateConversationAsync(
+                    teamsChannelId,
+                    serviceUrl,
+                    credentials,
+                    conversationParameters,
+                    async (t1, c1) =>
+                    {
+                        conversationReference = t1.Activity.GetConversationReference();
+                        await ((BotFrameworkAdapter)turnContext.Adapter).ContinueConversationAsync(
+                            _appId,
+                            conversationReference,
+                            async (t2, c2) =>
+                            {
+                                await t2.SendActivityAsync(proactiveMessage, c2);
+                            },
+                            cancellationToken);
+                    },
+                    cancellationToken);
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text("All messages have been sent."), cancellationToken);
+        }
+
+        private static async Task<List<TeamsChannelAccount>> GetPagedMembers(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var members = new List<TeamsChannelAccount>();
+            string continuationToken = null;
+
+            do
+            {
+                var currentPage = await TeamsInfo.GetPagedMembersAsync(turnContext, 100, continuationToken, cancellationToken);
+                continuationToken = currentPage.ContinuationToken;
+                members = members.Concat(currentPage.Members).ToList();
+            }
+            while (continuationToken != null);
+
+            return members;
+        }
+
+        private static async Task SendWelcomeCard(ITurnContext<IMessageActivity> turnContext, HeroCard card, CancellationToken cancellationToken)
+        {
+            var initialValue = new JObject { { "count", 0 } };
+            card.Title = "Welcome!";
+            card.Buttons.Add(new CardAction
+            {
+                Type = ActionTypes.MessageBack,
+                Title = "Update Card",
+                Text = "UpdateCardAction",
+                Value = initialValue
+            });
+
+            var activity = MessageFactory.Attachment(card.ToAttachment());
+
+            await turnContext.SendActivityAsync(activity, cancellationToken);
+        }
+
+        private static async Task SendUpdatedCard(ITurnContext<IMessageActivity> turnContext, HeroCard card, CancellationToken cancellationToken)
+        {
+            card.Title = "I've been updated";
+
+            var data = turnContext.Activity.Value as JObject;
+            data = JObject.FromObject(data);
+            data["count"] = data["count"].Value<int>() + 1;
+            card.Text = $"Update count - {data["count"].Value<int>()}";
+
+            card.Buttons.Add(new CardAction
+            {
+                Type = ActionTypes.MessageBack,
+                Title = "Update Card",
+                Text = "UpdateCardAction",
+                Value = data
+            });
+
+            var activity = MessageFactory.Attachment(card.ToAttachment());
+            activity.Id = turnContext.Activity.ReplyToId;
+
+            await turnContext.UpdateActivityAsync(activity, cancellationToken);
+        }
+
+        private async Task MentionActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var mention = new Mention
+            {
+                Mentioned = turnContext.Activity.From,
+                Text = $"<at>{XmlConvert.EncodeName(turnContext.Activity.From.Name)}</at>",
+            };
+
+            var replyActivity = MessageFactory.Text($"Hello {mention.Text}.");
+            replyActivity.Entities = new List<Entity> { mention };
+
+            await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+        }
+
+        private async Task RespondToThreadAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var teamsChannelId = turnContext.Activity.TeamsGetChannelId();
+            var message = MessageFactory.Text("This will start a new thread in a channel");
+
+            var serviceUrl = turnContext.Activity.ServiceUrl;
+            var credentials = new MicrosoftAppCredentials(_appId, _appPassword);
+
+            var conversationParameters = new ConversationParameters
+            {
+                IsGroup = true,
+                ChannelData = new { channel = new { id = teamsChannelId } },
+                Activity = (Activity)message,
+            };
+
+            ConversationReference conversationReference = null;
+
+            await ((BotFrameworkAdapter)turnContext.Adapter).CreateConversationAsync(
+                teamsChannelId,
+                serviceUrl,
+                credentials,
+                conversationParameters,
+                (t, ct) =>
+                {
+                    conversationReference = t.Activity.GetConversationReference();
+                    return Task.CompletedTask;
+                },
+                cancellationToken);
+
+
+            await ((BotFrameworkAdapter)turnContext.Adapter).ContinueConversationAsync(
+                _appId,
+                conversationReference,
+                async (t, ct) =>
+                {
+                    await t.SendActivityAsync(MessageFactory.Text("This will be the first response to the new thread"), ct);
+                },
+                cancellationToken);
+        }
+
+    }
+}
